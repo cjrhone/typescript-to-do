20d8d6e60265e5b1a689a7ae3576539179f64ecc
cj <charlesrhonejr@gmail.com>
2018-06-04 14:01:23 -0700

    Add notes in app.module.ts

---
+++ to-do/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+//imports code to run our app in the browser, allow us to add conditionals and loops to components
+import { NgModule } from '@angular/core';
+//imports general Module code from the Angular framework's core
+
+import { AppComponent } from './app.component';
+//application's root component
+
+
+@NgModule({
+  //informs Angular that this is a module
+  declarations: [
+    AppComponent
+  //declaration is an array of all components that will reside in this module.
+  //when we generate new components they'll be added here
+  ],
+  imports: [
+    BrowserModule
+  //imports other pieces of our application we want for this module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+  //bootstrap is array of components required upon launching application
+})
+export class AppModule { }
