bbad45c654595602cd6ff6b1584f5250d396308e
cj <charlesrhonejr@gmail.com>
2018-06-04 13:19:41 -0700

    Add more tasks to console.log and markDone function

--- app/to-do.js
+++ app/to-do.js
@@ -2,18 +2,31 @@ var Task = /** @class */ (function () {
     // description: string;
     // priority: string;
     function Task(description, priority) {
-        // this.done = false;
-        // this.description = description;
-        // this.priority = priority;
         this.description = description;
         this.priority = priority;
         this.done = false;
-        //constructor instantiates new tasks
     }
+    // this.done = false;
+    // this.description = description;
+    // this.priority = priority;
+    //constructor instantiates new tasks
+    Task.prototype.markDone = function () {
+        this.done = true;
+        //this runs markDone() which marks the done property from false to true upon instantion
+    };
+    ;
     return Task;
 }());
+;
 var tasks = [];
 // Task[] is an array data type
 tasks.push(new Task('Do the dishes.', 'Medium'));
 //Typescript requires strings be single quotes ('example')
-console.log(tasks);
+tasks.push(new Task('Buy Chocolate.', 'Low'));
+tasks.push(new Task('Do laundry.', 'High'));
+tasks[1].markDone();
+for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
+    var task = tasks_1[_i];
+    console.log(task);
+}
+;
--- app/to-do.ts
+++ app/to-do.ts
@@ -3,18 +3,29 @@ class Task {
   // description: string;
   // priority: string;
 
-  constructor(public description: string, public priority: string){
+  constructor(public description: string, public priority: string){}
     // this.done = false;
     // this.description = description;
     // this.priority = priority;
 
     //constructor instantiates new tasks
-  }
 
-}
+    markDone(){
+      this.done = true;
+      //this runs markDone() which marks the done property from false to true
+    };
+};
 
 var tasks: Task[] = [];
 // Task[] is an array data type
 tasks.push(new Task('Do the dishes.', 'Medium'));
 //Typescript requires strings be single quotes ('example')
-console.log(tasks);
+tasks.push(new Task('Buy Chocolate.', 'Low'));
+tasks.push(new Task('Do laundry.', 'High'));
+
+tasks[1].markDone();
+//marks the 2nd task as done
+
+for(var task of tasks){
+  console.log(task);
+};
